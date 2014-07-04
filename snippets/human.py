4c314466e52a14d585cb875539393cdb52e71de8
Dilawar <dilawars@iitb.ac.in>
2014-07-04 19:21:52 +0530

    Added class and objects.

---
+++ snippets/human.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  4 14:20:39 2014
+
+@author: dilawar
+"""
+
+class Human():
+    
+    def __init__(self):
+        self.name = None
+        
+        
+dilawar = Human()
